8b81e3db96c892872c86e7c1f406bd0569b37344
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-11-21 16:03:31 +0530

    refactor: add wrapper for opening file with application

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ bundlePath = hs.processInfo["bundlePath"]
 if hs.accessibilityState() == false then -- testing if hammerspoon has accessibility access.
     hs.osascript.applescript(
         [[tell application "System Events" to display dialog "Accessibility access is disabled which prevents LES from working properly." & return & "Please turn on accessibility access in" & return & "Preferences > Security & Privacy > Privacy > Accessibility, and try again." buttons {"Ok"} default button "Ok" with title "Live Enhancement Suite"]])
-    os.execute([[open /System/Library/PreferencePanes/Security.prefPane]])
+        ShellNSOpen("/System/Library/PreferencePanes/Security.prefPane")
     os.exit()
 end
 
